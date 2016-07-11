f8872c2f527810c7c44c0185d06699c596c0b994
pomia <pomia@gizwits.com>
2016-07-11 09:53:53 +0800

    update app info string

--- GOpenSourceAppKit/AppDelegate.h
+++ GOpenSourceAppKit/AppDelegate.h
@@ -10,13 +10,13 @@
 
 #warning - please replace your app info
 
-#define APP_ID  @"please_replace_your_app_id"
-#define APP_SECRET  @"please_replace_your_app_secret"
-#define PRODUCT_KEY @"please_replace_your_device_product_key"
+#define APP_ID  @"your_app_id"
+#define APP_SECRET  @"your_app_secret"
+#define PRODUCT_KEY @"your_product_key"
 
-#define TENCENT_APP_ID @"please_replace_your_tencent_app_id"
-#define JPUSH_APP_KEY @"please_replace_your_jpush_app_key"
-#define BPUSH_API_KEY @"please_replace_your_bpush_api_key"
+#define TENCENT_APP_ID @"your_tencent_app_id"
+#define JPUSH_APP_KEY @"your_jpush_app_key"
+#define BPUSH_API_KEY @"your_bpush_api_key"
 
 /******************** 推送开关 ********************/
 // 只能同时支持其中一种
